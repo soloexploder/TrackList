5affe04771768b7c1f381a84517e5d1733805c45
soloexploder <soloexploder@gmail.com>
2022-02-24 16:56:08 +0300

    Initial Commit

---
+++ TrackList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackList
+//
+//  Created by Даниил Хантуров on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
